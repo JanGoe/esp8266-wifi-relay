5cb8e92ce99427be2699c80f36c6092686fae9e0
JanGoe <jan.andrea7@googlemail.com>
2015-12-15 16:12:42 +0100

    Big Update
    tcp.php anpassung für update über wifi ( benötigt kein ttl-usb mehr um
    lua dateien zu "bearbeiten" )
    bsp: tcp.php 192.168.0.62 update datei.lua <- würde "datei.lua" auf den
    esp übertragen und anschliesend neustarten
    rename init.lua zu aktor.lua
    init.lua funktion eingefügt, die bei ersten start einen AP erstellt so
    ist es mittel smartphone möglich sich mit W-Lan SSID ESP_XXX zu
    verbinden und anschliesend über den browser "http://192.168.4.1/set" die
    SSID und das Password zu speichern
    servernode.lua für wlan update
    set.lua html für "set" für w-lan daten änderung
    wifi_tools.lua für wlan update siehe
    https://github.com/JanGoe/ESP8266_WiFi_File_Manager

---
+++ lua-tcp/set.lua
@@ -0,0 +1,14 @@
+<html>
+<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01//EN'>
+<head>
+<title>WiFi ESP8266 Manager</title>
+</head>
+<body bgcolor='#E6E6E6'>
+<h2><font face="Arial">ESP8266-ESP12e Wifi Setup</h2>
+<form action="/set1" method="GET">
+<input type="text" name="ssid" value="SSID"><br>
+<input type="text" name="pwd" value="Password"><br>
+<input type="Submit" name="senden" value="Speichern"><br><br><br>
+<font size="2" color="red">Achtung "Speichern" Startet den Chip direkt mit den eingegeben WLan Daten neu!</font>
+<font size="2" face="Arial"><a href="mailto:jan.andrea7@googlemail.com">(c) by Jan Gödecke 2015-2016</a>
+</html>
